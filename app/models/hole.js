db4fe8c44cee217d9802aecd72dea4c1a162c458
Grascon <gilberto.r.s@icloud.com>
2018-05-10 20:07:40 -0700

    Added js for game page to show user progress as they score themselves

--- app/models/hole.js
+++ app/models/hole.js
@@ -1,42 +1,26 @@
-module.exports = function (sequelize, Sequelize) {
-
-    var Hole = sequelize.define('hole', {
-        hole_id: {
-            autoIncrement: true, primaryKey: true, type: Sequelize.INTEGER
-        },
-        hole_number: {
-            type: Sequelize.INTEGER
-        },
-        par: {
-            type: Sequelize.INTEGER
-        },
-        hole_distance: {
-            type: Sequelize.INTEGER
-        },
+module.exports = function(sequelize, DataTypes) {
+    var Hole = sequelize.define("Hole", {
+      hole_number: {
+        type: DataTypes.INTEGER,
+      },
+      par: {
+        type: DataTypes.INTEGER,
+      },
+      hole_distance: {
+        type: DataTypes.INTEGER,
+      }
     }, {
-            timestamps: false,
-
-            // Inside the option object, we need a property called "classMethod".
-            // This is an object too.
-            classMethods: {
-
-                associate: function (models) {
-                    // The models parameter is the db object in index.js.
-                    // Line 24 in index.js calls this method and passes it
-                    // the db object.
-                    // This is were we define the association for this model.
-                    Hole.belongsTo(models.CustomCourse, {
-                        // This object is options. You can omit this
-                        // parameter and the association will be created
-                        // from index.js
-                        as: 'CustomCourse',
-                        foreignKey: 'custom_courseID'
-                    });
-                    Hole.hasMany(models.Holescore, { as: "HoleScores" })
-                }
+      timestamps: false
+    });
+  
+    Hole.associate = function(models) {
+        Hole.belongsTo(models.CustomCourse, {
+            foreignKey: {
+            allowNull: false
             }
         });
-
+    };
+  
     return Hole;
-
-}
+  };
+  
